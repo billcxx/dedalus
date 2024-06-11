faa8c3e77fe760d6ca8deab4749abf0792fc808a
Xiaoxing Cheng <hello@chengxiaoxing.me>
2024-06-11 18:08:39 +0800

    update

--- fenics/fenicsx.py
+++ fenics/fenicsx.py
@@ -3,25 +3,33 @@ from mpi4py import MPI
 from petsc4py import PETSc
 from dolfinx import fem, mesh
 from dolfinx.io import XDMFFile
+import numpy as np
+from basix.ufl import element
 
 # Define the domain
 Lx, Ly = 2.0, 1.0
 nx, ny = 32, 16
 domain = mesh.create_rectangle(MPI.COMM_WORLD, [[0.0, 0.0], [Lx, Ly]], [nx, ny])
+gdim = domain.topology.dim
 
 # Function spaces
-V = fem.VectorFunctionSpace(domain, ("Lagrange", 1))
-Q = fem.FunctionSpace(domain, ("Lagrange", 1))
+P2 = element("Lagrange", domain.topology.cell_name(), 2, shape=(domain.geometry.dim, ))
+P1 = element("Lagrange", domain.topology.cell_name(), 1)
+V = fem.functionspace(domain,P2)
+Q = fem.functionspace(domain,P1)
 
+#
+
+fdim = gdim - 1
 # Boundary conditions
 def inlet_boundary(x):
-    return x[0] == 0.0
+    return np.isclose(x[0],0.0)
 
 def outlet_boundary(x):
-    return x[0] == Lx
+    return np.isclose(x[0], Lx)
 
 def wall_boundary(x):
-    return x[1] == 0.0 or x[1] == Ly
+    return np.isclose(x[1],0.0) or np.isclose(x[1],Ly)
 
 inlet_velocity = fem.Constant(domain, [1.0, 0.0])
 bc_inlet = fem.dirichletbc(inlet_velocity, fem.locate_dofs_geometrical(V, inlet_boundary))
@@ -43,10 +51,10 @@ a2 = ufl.inner(ufl.grad(p), ufl.grad(q)) * ufl.dx
 L2 = -ufl.inner(ufl.div(u), q) * ufl.dx
 
 # Assemble
-A1 = form(a1)
-b1 = form(L1)
-A2 = form(a2)
-b2 = form(L2)
+A1 = fem.form(a1)
+b1 = fem.form(L1)
+A2 = fem.form(a2)
+b2 = fem.form(L2)
 
 # Solve
 solver1 = fem.petsc.LinearProblem(A1, b1, bcs=[bc_inlet, bc_wall])
